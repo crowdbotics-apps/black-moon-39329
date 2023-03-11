f99b5d8377e6b3d49ca36de0e7538971ff447967
Crowdbotics <team@crowdbotics.com>
2023-03-11 18:30:44 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "black-moon-39329",
+  url: "https://black-moon-39329.botics.co",
+  api: "https://black-moon-39329.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
